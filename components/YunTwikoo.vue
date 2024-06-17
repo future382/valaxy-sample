ebe1db8bfd2352835de2e689aae7b3997f59d5c9
future <96325750+future382@users.noreply.github.com>
2024-06-17 13:04:35 +0800

    Create YunTwikoo.vue

---
+++ components/YunTwikoo.vue
@@ -0,0 +1,17 @@
+<!-- YunTwikoo.vue -->
+<script lang="ts" setup>
+import { useTwikooWithOptions } from 'valaxy-addon-twikoo'
+
+// twikoo loading style patch
+import 'valaxy-addon-twikoo/client/styles/index.scss'
+
+useTwikooWithOptions()
+</script>
+
+<template>
+  <div id="tcomment" w="full" />
+</template>
+
+<style lang="scss">
+// custom twikoo style
+</style>
